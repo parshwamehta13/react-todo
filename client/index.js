251690b31aa961a75326ae871b41bf5362f53f9e
Parshwa Mehta <parshwa.mehta@innovaccer.com>
2018-02-13 12:09:03 +0530

    Adds bootstrap styling. Adds css-loader and style-loader in webpack-config.js

--- client/index.js
+++ client/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import TodoApp from "./components/TodoApp.jsx";
+import styles from "./styles/bootstrap/bootstrap.css"
+
 ReactDOM.render(<TodoApp />, document.getElementById("root"));
